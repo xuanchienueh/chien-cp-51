f20c5bc1d4b143447fa2da6e716961362c434020
xuanchienueh <xuanchien.ueh@gmail.com>
2022-10-07 03:31:02 +0700

    3

--- js/statistical.js
+++ js/statistical.js
@@ -1,232 +1,263 @@
 window.onload = function () {
-    spinner1(67)
-    spinner2(89)
-    spinner3(74)
-    spinner4(77)
+  spinner0(67);
+  spinner1(89);
+  spinner2(74);
+  spinner3(77);
+};
+const class_spinners = document.querySelectorAll("#statistical .spinner");
+// class_spinners.forEach((class_spinner, index) => {
+//   class_spinners[index].onmouseover = function () {
+//     let nameFunction = "spinner" + index;
+//     console.log(nameFunction);
+
+//   };
+// });
+class_spinners[0].onmouseover = function () {
+  spinner0(67);
+};
+class_spinners[1].onmouseover = function () {
+  spinner1(89);
+};
+class_spinners[2].onmouseover = function () {
+  spinner2(74);
+};
+class_spinners[3].onmouseover = function () {
+  spinner3(77);
+};
+function spinner0(percent) {
+  let spinner = document.getElementById("spinner1");
+
+  let ctx = spinner.getContext("2d");
+  let width = spinner.width;
+  let height = spinner.height;
+  let degrees = 0;
+  let new_degrees = 0;
+  let difference = 0;
+  let color = "#0096dc";
+  let bgcolor = "#cecfd0";
+  let text;
+  let animation_loop, redraw_loop;
+
+  function init() {
+    ctx.clearRect(0, 0, width, height);
+
+    ctx.beginPath();
+    ctx.strokeStyle = bgcolor;
+    ctx.lineWidth = 50;
+    ctx.arc(width / 2, width / 2, 100, 0, Math.PI * 2, false);
+    ctx.stroke();
+    let radians = (degrees * Math.PI) / 180;
+
+    ctx.beginPath();
+    ctx.strokeStyle = color;
+    ctx.lineWidth = 50;
+    ctx.arc(
+      width / 2,
+      height / 2,
+      100,
+      0 - (90 * Math.PI) / 180,
+      radians - (90 * Math.PI) / 180,
+      false
+    );
+    ctx.stroke();
+    ctx.fillStyle = color;
+    ctx.font = "40px arial";
+    text = Math.floor((degrees / 360) * 100) + "%";
+    text_width = ctx.measureText(text).width;
+    ctx.fillText(text, width / 2 - text_width / 2, height / 2 + 15);
+  }
+
+  function draw(percent) {
+    if (typeof animation_loop != undefined) clearInterval(animation_loop);
+    new_degrees = Math.ceil(percent * 3.6);
+    difference = new_degrees - degrees;
+    animation_loop = setInterval(animate_to, 300 / difference);
+  }
+
+  function animate_to() {
+    if (degrees == new_degrees) clearInterval(animation_loop);
+    else if (degrees < new_degrees) degrees++;
+    else degrees--;
+    init();
+  }
+
+  draw(percent);
 }
 
-function spinner1(percent){ 
+function spinner1(percent) {
+  let spinner = document.getElementById("spinner2");
 
-    let spinner = document.getElementById("spinner1");
+  let ctx = spinner.getContext("2d");
+  let width = spinner.width;
+  let height = spinner.height;
+  let degrees = 0;
+  let new_degrees = 0;
+  let difference = 0;
+  let color = "#0096dc";
+  let bgcolor = "#cecfd0";
+  let text;
+  let animation_loop, redraw_loop;
 
-    let ctx = spinner.getContext("2d");
-    let width = spinner.width;
-    let height = spinner.height;
-    let degrees = 0;
-    let new_degrees = 0;
-    let difference = 0;
-    let color = "#0096dc";
-    let bgcolor = "#cecfd0";
-    let text;
-    let animation_loop, redraw_loop;
+  function init() {
+    ctx.clearRect(0, 0, width, height);
 
-    function init() {
-        ctx.clearRect(0, 0, width, height);
+    ctx.beginPath();
+    ctx.strokeStyle = bgcolor;
+    ctx.lineWidth = 50;
+    ctx.arc(width / 2, width / 2, 100, 0, Math.PI * 2, false);
+    ctx.stroke();
+    let radians = (degrees * Math.PI) / 180;
 
-        ctx.beginPath();
-        ctx.strokeStyle = bgcolor;
-        ctx.lineWidth = 30;
-        ctx.arc(width / 2, width / 2, 100, 0, Math.PI * 2, false);
-        ctx.stroke();
-        let radians = degrees * Math.PI / 180;
+    ctx.beginPath();
+    ctx.strokeStyle = color;
+    ctx.lineWidth = 50;
+    ctx.arc(
+      width / 2,
+      height / 2,
+      100,
+      0 - (90 * Math.PI) / 180,
+      radians - (90 * Math.PI) / 180,
+      false
+    );
+    ctx.stroke();
+    ctx.fillStyle = color;
+    ctx.font = "40px arial";
+    text = Math.floor((degrees / 360) * 100) + "%";
+    text_width = ctx.measureText(text).width;
+    ctx.fillText(text, width / 2 - text_width / 2, height / 2 + 15);
+  }
 
-        ctx.beginPath();
-        ctx.strokeStyle = color;
-        ctx.lineWidth = 30;
-        ctx.arc(width / 2, height / 2, 100, 0 - 90 * Math.PI / 180, radians - 90 * Math.PI / 180, false);
-        ctx.stroke();
-        ctx.fillStyle = color;
-        ctx.font = "40px arial";
-        text = Math.floor(degrees / 360 * 100) + "%";
-        text_width = ctx.measureText(text).width;
-        ctx.fillText(text, width / 2 - text_width / 2, height / 2 + 30);
-    }
+  function draw(percent) {
+    if (typeof animation_loop != undefined) clearInterval(animation_loop);
+    new_degrees = Math.ceil(percent * 3.6);
+    difference = new_degrees - degrees;
+    animation_loop = setInterval(animate_to, 300 / difference);
+  }
 
-    function draw(percent) {
-        if (typeof animation_loop != undefined) clearInterval(animation_loop);
-        new_degrees = Math.ceil(percent * 3.6);
-        difference = new_degrees - degrees;
-        animation_loop = setInterval(animate_to, 1000 / difference);
-    }
+  function animate_to() {
+    if (degrees == new_degrees) clearInterval(animation_loop);
+    else if (degrees < new_degrees) degrees++;
+    else degrees--;
+    init();
+  }
 
-    function animate_to() {
-        if (degrees == new_degrees)
-            clearInterval(animation_loop);
-        else if (degrees < new_degrees)
-            degrees++;
-        else
-            degrees--;
-        init();
-    }
-
-    draw(percent);
+  draw(percent);
 }
+function spinner2(percent) {
+  let spinner = document.getElementById("spinner3");
 
-function spinner2 (percent){ 
+  let ctx = spinner.getContext("2d");
+  let width = spinner.width;
+  let height = spinner.height;
+  let degrees = 0;
+  let new_degrees = 0;
+  let difference = 0;
+  let color = "#0096dc";
+  let bgcolor = "#cecfd0";
+  let text;
+  let animation_loop, redraw_loop;
 
-    let spinner = document.getElementById("spinner2");
+  function init() {
+    ctx.clearRect(0, 0, width, height);
 
-    let ctx = spinner.getContext("2d");
-    let width = spinner.width;
-    let height = spinner.height;
-    let degrees = 0;
-    let new_degrees = 0;
-    let difference = 0;
-    let color = "#0096dc";
-    let bgcolor = "#cecfd0";
-    let text;
-    let animation_loop, redraw_loop;
+    ctx.beginPath();
+    ctx.strokeStyle = bgcolor;
+    ctx.lineWidth = 50;
+    ctx.arc(width / 2, width / 2, 100, 0, Math.PI * 2, false);
+    ctx.stroke();
+    let radians = (degrees * Math.PI) / 180;
 
-    function init() {
-        ctx.clearRect(0, 0, width, height);
+    ctx.beginPath();
+    ctx.strokeStyle = color;
+    ctx.lineWidth = 50;
+    ctx.arc(
+      width / 2,
+      height / 2,
+      100,
+      0 - (90 * Math.PI) / 180,
+      radians - (90 * Math.PI) / 180,
+      false
+    );
+    ctx.stroke();
+    ctx.fillStyle = color;
+    ctx.font = "40px arial";
+    text = Math.floor((degrees / 360) * 100) + "%";
+    text_width = ctx.measureText(text).width;
+    ctx.fillText(text, width / 2 - text_width / 2, height / 2 + 15);
+  }
 
-        ctx.beginPath();
-        ctx.strokeStyle = bgcolor;
-        ctx.lineWidth = 30;
-        ctx.arc(width / 2, width / 2, 100, 0, Math.PI * 2, false);
-        ctx.stroke();
-        let radians = degrees * Math.PI / 180;
+  function draw(percent) {
+    if (typeof animation_loop != undefined) clearInterval(animation_loop);
+    new_degrees = Math.ceil(percent * 3.6);
+    difference = new_degrees - degrees;
+    animation_loop = setInterval(animate_to, 300 / difference);
+  }
 
-        ctx.beginPath();
-        ctx.strokeStyle = color;
-        ctx.lineWidth = 30;
-        ctx.arc(width / 2, height / 2, 100, 0 - 90 * Math.PI / 180, radians - 90 * Math.PI / 180, false);
-        ctx.stroke();
-        ctx.fillStyle = color;
-        ctx.font = "40px arial";
-        text = Math.floor(degrees / 360 * 100) + "%";
-        text_width = ctx.measureText(text).width;
-        ctx.fillText(text, width / 2 - text_width / 2, height / 2 + 30);
-    }
+  function animate_to() {
+    if (degrees == new_degrees) clearInterval(animation_loop);
+    else if (degrees < new_degrees) degrees++;
+    else degrees--;
+    init();
+  }
 
-    function draw(percent) {
-        if (typeof animation_loop != undefined) clearInterval(animation_loop);
-        new_degrees = Math.ceil(percent * 3.6);
-        difference = new_degrees - degrees;
-        animation_loop = setInterval(animate_to, 1000 / difference);
-    }
-
-    function animate_to() {
-        if (degrees == new_degrees)
-            clearInterval(animation_loop);
-        else if (degrees < new_degrees)
-            degrees++;
-        else
-            degrees--;
-        init();
-    }
-
-    draw(percent);
+  draw(percent);
 }
-function spinner3 (percent){ 
+function spinner3(percent) {
+  let spinner = document.getElementById("spinner4");
 
-    let spinner = document.getElementById("spinner3");
+  let ctx = spinner.getContext("2d");
+  let width = spinner.width;
+  let height = spinner.height;
+  let degrees = 0;
+  let new_degrees = 0;
+  let difference = 0;
+  let color = "#0096dc";
+  let bgcolor = "#cecfd0";
+  let text;
+  let animation_loop, redraw_loop;
 
-    let ctx = spinner.getContext("2d");
-    let width = spinner.width;
-    let height = spinner.height;
-    let degrees = 0;
-    let new_degrees = 0;
-    let difference = 0;
-    let color = "#0096dc";
-    let bgcolor = "#cecfd0";
-    let text;
-    let animation_loop, redraw_loop;
+  function init() {
+    ctx.clearRect(0, 0, width, height);
 
-    function init() {
-        ctx.clearRect(0, 0, width, height);
+    ctx.beginPath();
+    ctx.strokeStyle = bgcolor;
+    ctx.lineWidth = 50;
+    ctx.arc(width / 2, width / 2, 100, 0, Math.PI * 2, false);
+    ctx.stroke();
+    let radians = (degrees * Math.PI) / 180;
 
-        ctx.beginPath();
-        ctx.strokeStyle = bgcolor;
-        ctx.lineWidth = 30;
-        ctx.arc(width / 2, width / 2, 100, 0, Math.PI * 2, false);
-        ctx.stroke();
-        let radians = degrees * Math.PI / 180;
+    ctx.beginPath();
+    ctx.strokeStyle = color;
+    ctx.lineWidth = 50;
+    ctx.arc(
+      width / 2,
+      height / 2,
+      100,
+      0 - (90 * Math.PI) / 180,
+      radians - (90 * Math.PI) / 180,
+      false
+    );
+    ctx.stroke();
+    ctx.fillStyle = color;
+    ctx.font = "40px arial";
+    text = Math.floor((degrees / 360) * 100) + "%";
+    text_width = ctx.measureText(text).width;
+    ctx.fillText(text, width / 2 - text_width / 2, height / 2 + 15);
+  }
 
-        ctx.beginPath();
-        ctx.strokeStyle = color;
-        ctx.lineWidth = 30;
-        ctx.arc(width / 2, height / 2, 100, 0 - 90 * Math.PI / 180, radians - 90 * Math.PI / 180, false);
-        ctx.stroke();
-        ctx.fillStyle = color;
-        ctx.font = "40px arial";
-        text = Math.floor(degrees / 360 * 100) + "%";
-        text_width = ctx.measureText(text).width;
-        ctx.fillText(text, width / 2 - text_width / 2, height / 2 + 30);
-    }
+  function draw(percent) {
+    if (typeof animation_loop != undefined) clearInterval(animation_loop);
+    new_degrees = Math.ceil(percent * 3.6);
+    difference = new_degrees - degrees;
+    animation_loop = setInterval(animate_to, 300 / difference);
+  }
 
-    function draw(percent) {
-        if (typeof animation_loop != undefined) clearInterval(animation_loop);
-        new_degrees = Math.ceil(percent * 3.6);
-        difference = new_degrees - degrees;
-        animation_loop = setInterval(animate_to, 1000 / difference);
-    }
+  function animate_to() {
+    if (degrees == new_degrees) clearInterval(animation_loop);
+    else if (degrees < new_degrees) degrees++;
+    else degrees--;
+    init();
+  }
 
-    function animate_to() {
-        if (degrees == new_degrees)
-            clearInterval(animation_loop);
-        else if (degrees < new_degrees)
-            degrees++;
-        else
-            degrees--;
-        init();
-    }
-
-    draw(percent);
-}
-function spinner4 (percent){ 
-
-    let spinner = document.getElementById("spinner4");
-
-    let ctx = spinner.getContext("2d");
-    let width = spinner.width;
-    let height = spinner.height;
-    let degrees = 0;
-    let new_degrees = 0;
-    let difference = 0;
-    let color = "#0096dc";
-    let bgcolor = "#cecfd0";
-    let text;
-    let animation_loop, redraw_loop;
-
-    function init() {
-        ctx.clearRect(0, 0, width, height);
-
-        ctx.beginPath();
-        ctx.strokeStyle = bgcolor;
-        ctx.lineWidth = 30;
-        ctx.arc(width / 2, width / 2, 100, 0, Math.PI * 2, false);
-        ctx.stroke();
-        let radians = degrees * Math.PI / 180;
-
-        ctx.beginPath();
-        ctx.strokeStyle = color;
-        ctx.lineWidth = 30;
-        ctx.arc(width / 2, height / 2, 100, 0 - 90 * Math.PI / 180, radians - 90 * Math.PI / 180, false);
-        ctx.stroke();
-        ctx.fillStyle = color;
-        ctx.font = "40px arial";
-        text = Math.floor(degrees / 360 * 100) + "%";
-        text_width = ctx.measureText(text).width;
-        ctx.fillText(text, width / 2 - text_width / 2, height / 2 + 30);
-    }
-
-    function draw(percent) {
-        if (typeof animation_loop != undefined) clearInterval(animation_loop);
-        new_degrees = Math.ceil(percent * 3.6);
-        difference = new_degrees - degrees;
-        animation_loop = setInterval(animate_to, 1000 / difference);
-    }
-
-    function animate_to() {
-        if (degrees == new_degrees)
-            clearInterval(animation_loop);
-        else if (degrees < new_degrees)
-            degrees++;
-        else
-            degrees--;
-        init();
-    }
-
-    draw(percent);
+  draw(percent);
 }
