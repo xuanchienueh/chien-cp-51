aeb7c5c49b16ee454ff97cde860bc54f33b006b0
xuanchienueh <xuanchien.ueh@gmail.com>
2022-10-07 17:44:30 +0700

    6

--- js/track_tabs.js
+++ js/track_tabs.js
@@ -7,27 +7,9 @@ const panes = $$("#outcomes_study .track_tabs .tab-pane");
 
 const arrow_left = $('#outcomes_study .track_tabs .arrow_left')
 const arrow_right = $('#outcomes_study .track_tabs .arrow_right')
-
-// if(number_index < tabs.length -1) {
-//   arrow_left.classList.remove('active');
-//   arrow_right.classList.add('active');
-// }
-
-// if (number_index >0 && number_index <= tabs.length -2) { 
-//   arrow_left.classList.add('active');
-//   arrow_right.classList.add('active');
-// } else if(number_index === tabs.length -2){ 
-//   arrow_right.classList.remove('active');
-
-// }
-// if(number_index === tabs.length ){ 
-//   arrow_left.classList.add('active');
-//   arrow_right.classList.remove('active');
-// }
-
-
-
 const tabActive = $("#outcomes_study .track_tabs .tab-item.active");
+const tab_content = $("#outcomes_study .track_tabs .tab-content")
+
 
 tabs.forEach((tab, index) => {
   const pane = panes[index];
@@ -39,6 +21,7 @@ tabs.forEach((tab, index) => {
     tab.classList.add("active");
     pane.classList.add("active");
     number_index = index;
+    console.log(number_index)
   };
 });
 
@@ -51,29 +34,74 @@ arrow_left.onclick = function () {
     tabs[number_index].classList.add("active");
     panes[number_index].classList.add("active");
   }
-  if(number_index === 0 ){ 
+  if (number_index === 0) {
     arrow_left.classList.remove("active");
-  } 
-  if(number_index > 0 && number_index < tabs.length -1) { 
+  }
+  if (number_index > 0 && number_index < tabs.length - 1) {
     arrow_right.classList.add("active");
     arrow_left.classList.add("active");
   }
+  console.log('arrow left',number_index)
+
 }
 
 arrow_right.onclick = function () {
   number_index = number_index + 1;
-  if (number_index > tabs.length -1) { number_index = tabs.length -1}
-  if (number_index < tabs.length) { 
+  if (number_index >= tabs.length - 1) { number_index = tabs.length - 1 }
+  if (number_index < tabs.length) {
     $("#outcomes_study .track_tabs .tab-item.active").classList.remove("active");
     $("#outcomes_study .track_tabs .tab-pane.active").classList.remove("active");
     tabs[number_index].classList.add("active");
     panes[number_index].classList.add("active");
   }
-  if (number_index === tabs.length -1){ 
+  if (number_index === tabs.length - 1) {
     arrow_right.classList.remove("active");
   }
-  if(number_index > 0 && number_index < tabs.length -1) { 
+  if (number_index > 0 && number_index < tabs.length - 1) {
     arrow_right.classList.add("active");
     arrow_left.classList.add("active");
   }
+  console.log('arrow_right', number_index)
+
+}
+
+function myTimer() {
+  arrow_right.click()
+  if (number_index === tabs.length - 1) {
+    const a = setTimeout(() => {
+      arrow_left.click()
+      arrow_left.click()
+      arrow_left.click()
+     },2000)
+    
+  }
+}
+
+function myTimer2() {
+  
+}
+let myVar = setInterval(myTimer, 2000);
+let myVar2 = setInterval(myTimer2, 12000)
+
+
+
+// tab_content.onmouseover = () => {
+//   console.log(24)
+//   clearInterval(myVar)
+//   clearInterval(myVar2)
+// }
+
+tab_content.onmouseout = () => {
+  console.log(54)
+  // let a = setInterval(() => { 
+  //   arrow_right.click()
+  // }, 6000)
+  // let a2 = setInterval(() => {
+  //   if (number_index === tabs.length - 1) {
+  //     arrow_left.click()
+  //     arrow_left.click()
+  //     arrow_left.click()
+  //   }
+  //  }, 12000)
+ 
 }
