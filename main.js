2fc85d14cdd26954d6132af8c7b7a5d9563a48c5
Nguyễn Xuân Chiến <97378366+xuanchienueh@users.noreply.github.com>
2022-10-09 06:58:13 +0700

    7

--- main.js
+++ main.js
@@ -1,30 +1,60 @@
 
-const navItems = document.querySelectorAll(".system_study .desktop .nav-link");
 const sections = document.querySelectorAll(".system_study .desktop .section");
+const system_study = document.querySelector('.system_study')
+const navItems = document.querySelectorAll(".system_study .desktop .nav-link");
+const class_navItems = document.querySelectorAll(".system_study .desktop .nav-item")
+function getOffset(el) {
+    const rect = el.getBoundingClientRect();
+    return rect.top + window.scrollY
+  }
 
-window.addEventListener(  "scroll",  function () {
+let bodyRect = document.body.getBoundingClientRect();
 
+
+window.addEventListener(
+  "scroll",
+  function () {
+    let checkToView = null
+    if(document.querySelector(".system_study .desktop")){
+      checkToView = document
+      .querySelector(".system_study .desktop")
+      .getBoundingClientRect().top;
+       }
+   
+      // console.log( 'checkToView', checkToView )
+    if (checkToView !== null) {
+      if (checkToView < 0 && checkToView > -700) {
+        document.querySelector(".system_study .desktop .left").style.position = "fixed";
+        document.querySelector('.system_study .desktop .left').style.transform = 'scale(0.8) translateX(-30px)'
+        class_navItems[class_navItems.length-1].style.top = '7vh'
+
+      } else if (checkToView < -700) {
+        document.querySelector(".system_study .desktop .left").style.position = "relative";
+        document.querySelector('.system_study .desktop .left').style.transform = 'scale(0.8) ';
+        class_navItems[class_navItems.length-1].style.top = '70vh'
+
+      } else if (checkToView > 0) {
+          document.querySelector(".system_study .desktop .left").style.position = "unset";
+        document.querySelector('.system_study .desktop .left').style.transform = 'scale(0.8)';
+      }
+    }
     sections.forEach((section, index) => {
-      
       const height = section.offsetHeight;
-      const top = section.offsetTop + 400 ;
-      const html = document.documentElement;
+      let top = Math.floor(getOffset(section)) - 300 ;
 
+      const html = document.documentElement;
 
       navItems.forEach((navItem) => {
         const hrefNav = navItem.href.match(/#[a-zA-Z]+/)[0];
-        
+
         const idSection = "#" + section.id;
-        if (html.scrollTop >= top && top + height >= html.scrollTop) {
+        // console.log( 'top: ' + top )
+        // console.log( 'html.scrollTop: ' + html.scrollTop)
+        if (html.scrollTop >= top) {
           hrefNav === idSection
             ? navItem.parentElement.classList.add("active")
             : navItem.parentElement.classList.remove("active");
-        } else {
-          // hrefNav === idSection &&
-          //   navItem.parentElement.classList.remove("active");
-        }
-
-      
+        }         
       });
     });
   },
