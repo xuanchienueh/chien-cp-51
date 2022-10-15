f5368197b96331bce5389b7d8e575e157cbcaed5
xuanchienueh <xuanchien.ueh@gmail.com>
2022-10-15 17:10:11 +0700

    save

--- js/main.js
+++ js/main.js
@@ -1,29 +1,27 @@
 let procedureArr = [
-    {
-      title: "  Learn the skills to become a Full Stack Software Developer ",
-    },
-    {
-      title:
-        " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, vero?",
-    },
-    {
-      title:
-        " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, vero?",
-    },
-  ];
-  
-  let createItemProcedure = (data) => {
-    let contentHTMl = `<div cla-=>
+  {
+    title: "  Learn the skills to become a Full Stack Software Developer ",
+  },
+  {
+    title:
+      " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, vero?",
+  },
+  {
+    title:
+      " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, vero?",
+  },
+];
+
+let createItemProcedure = (data) => {
+  let contentHTMl = `<div cla-=>
    </div>`;
-  };
-  
-  function handleShowProcedure(e) {
-    // console.log(e.target);
-    // console.log(event.currentTarget);
-    let descEl = event.currentTarget.querySelector(".desc");
-    let lineEl = event.currentTarget.querySelector(".line");
-    console.log("descEl: ", descEl);
-    descEl.classList.toggle("active");
-    lineEl.classList.toggle("active");
-  }
-  
+};
+
+function handleShowProcedure(e) {
+  // console.log(e.target);
+  // console.log(event.currentTarget);
+  let descEl = event.currentTarget.querySelector("#procedure .desc");
+  let lineEl = event.currentTarget.querySelector("#procedure .line");
+  descEl && descEl.classList.toggle("active");
+  lineEl && lineEl.classList.toggle("active");
+}
