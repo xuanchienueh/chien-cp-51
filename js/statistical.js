75bb3891d826978bf7f6a3e66091cdad7c659658
xuanchienueh <xuanchien.ueh@gmail.com>
2022-10-15 22:02:42 +0700

    thêm 2 cái phần trăm mới( tổng 6)

--- js/statistical.js
+++ js/statistical.js
@@ -1,3 +1,4 @@
+const itemPercents = document.querySelectorAll('#statistical .container .bg')
 const c1 = document.querySelector("#statistical #c1");
 const t1 = document.querySelector("#statistical #t1");
 const c2 = document.querySelector("#statistical #c2");
@@ -6,36 +7,35 @@ const c3 = document.querySelector("#statistical #c3");
 const t3 = document.querySelector("#statistical #t3");
 const c4 = document.querySelector("#statistical #c4");
 const t4 = document.querySelector("#statistical #t4");
+const c5 = document.querySelector("#statistical #c5");
+const t5 = document.querySelector("#statistical #t5");
+const c6 = document.querySelector("#statistical #c6");
+const t6 = document.querySelector("#statistical #t6");
 const number_percent1 = document.querySelector("#statistical .container .number_percent1");
 const number_percent2 = document.querySelector("#statistical .container .number_percent2");
 const number_percent3 = document.querySelector("#statistical .container .number_percent3");
 const number_percent4 = document.querySelector("#statistical .container .number_percent4");
+const number_percent5 = document.querySelector("#statistical .container .number_percent5");
+const number_percent6 = document.querySelector("#statistical .container .number_percent6");
+const itemPercents0 = Math.floor(getOffset(itemPercents[0]));
+const itemPercents2 = Math.floor(getOffset(itemPercents[2]));
+const itemPercents4 = Math.floor(getOffset(itemPercents[4]));
+
 let valuePercent1 = 67
 let valuePercent2 = 77
 let valuePercent3 = 87
 let valuePercent4 = 97
+let valuePercent5 = 971
+let valuePercent6 = 90
 let percent_start = 0;
 const html = document.documentElement;
-const itemPercents = document.querySelectorAll('#statistical .container .bg')
 
-function getOffset(el) {
-  if ( el !== undefined ){ 
-    const rect = el.getBoundingClientRect();
 
-    return rect.top + window.scrollY
-  }
-}
-const itemPercents0 = Math.floor(getOffset(itemPercents[0]));
-const itemPercents2 = Math.floor(getOffset(itemPercents[2]));
-// console.log(itemPercents0, itemPercents2)
 let itemPercent_height = itemPercents && itemPercents[0] && itemPercents[0].clientHeight
 
 
 window.onload = () => {
-  animateNumber(valuePercent1, 1000, 0, percent1);
-  animateNumber(valuePercent2, 1000, 0, percent2);
-  animateNumber(valuePercent3, 1000, 0, percent3);
-  animateNumber(valuePercent4, 1000, 0, percent4);
+  onScroll()
   let timeOutId = setTimeout(() => {
     window.addEventListener("scroll", () => {
 
@@ -71,15 +71,7 @@ window.onload = () => {
   }, 2000)
 }
 
-function debounce(func, timeout = 500) {
-  let timer;
-  return (...args) => {
-    clearTimeout(timer);
-    timer = setTimeout(() => {
-      func.apply(this, args);
-    }, timeout);
-  };
-}
+
 function onScroll() {
   animateNumber(valuePercent1, 1000, 0, percent1);
   animateNumber(valuePercent2, 1000, 0, percent2);
@@ -100,6 +92,26 @@ var processChange1 = debounce(() => onScroll1());
 var processChange2 = debounce(() => onScroll2());
 var processChange = debounce(() => onScroll());
 
+
+
+
+
+function percent1(number) {
+  c1 ? c1.style.strokeDasharray = `${number} 100` : null;
+  number_percent1 ? number_percent1.innerHTML = number + "%" : null;
+}
+function percent2(number) {
+  c2 ? c2.style.strokeDasharray = `${number} 100` : null;
+  number_percent2 ? number_percent2.innerHTML = number + "%" : null
+}
+function percent3(number) {
+  c3 ? c3.style.strokeDasharray = `${number} 100` : null;
+  number_percent3 ? number_percent3.innerHTML = number + "%" : null
+}
+function percent4(number) {
+  c4 ? c4.style.strokeDasharray = `${number} 100` : null;
+  number_percent4 ? number_percent4.innerHTML = number + "%" : null
+}
 
 
 function animateNumber(
@@ -128,20 +140,20 @@ function animateNumber(
   }
 }
 
-function percent1(number) {
-  c1 ? c1.style.strokeDasharray = `${number} 100` : null;
-  number_percent1 ? number_percent1.innerHTML = number + "%" : null;
-}
-function percent2(number) {
-  c2 ? c2.style.strokeDasharray = `${number} 100` : null;
-  number_percent2 ? number_percent2.innerHTML = number + "%" : null
-}
-function percent3(number) {
-  c3 ? c3.style.strokeDasharray = `${number} 100` : null;
-  number_percent3 ? number_percent3.innerHTML = number + "%" : null
-}
-function percent4(number) {
-  c4 ? c4.style.strokeDasharray = `${number} 100` : null;
-  number_percent4 ? number_percent4.innerHTML = number + "%" : null
+function debounce(func, timeout = 500) {
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func.apply(this, args);
+    }, timeout);
+  };
 }
 
+function getOffset(el) {
+  if (el !== undefined) {
+    const rect = el.getBoundingClientRect();
+
+    return rect.top + window.scrollY
+  }
+}
