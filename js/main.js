15b58692c5c9c3883255c9cfe397a1d6aa5e0444
xuanchienueh <xuanchien.ueh@gmail.com>
2022-10-19 04:56:35 +0700

    save

--- js/main.js
+++ js/main.js
@@ -1,27 +1,18 @@
-let procedureArr = [
-  {
-    title: "  Learn the skills to become a Full Stack Software Developer ",
-  },
-  {
-    title:
-      " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, vero?",
-  },
-  {
-    title:
-      " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, vero?",
-  },
-];
-
-let createItemProcedure = (data) => {
-  let contentHTMl = `<div cla-=>
-   </div>`;
-};
 
 function handleShowProcedure(e) {
-  // console.log(e.target);
-  // console.log(event.currentTarget);
   let descEl = event.currentTarget.querySelector("#procedure .desc");
   let lineEl = event.currentTarget.querySelector("#procedure .line");
   descEl && descEl.classList.toggle("active");
   lineEl && lineEl.classList.toggle("active");
 }
+
+function page_doanh_nghiep_procedure() {
+  const skills = document.querySelectorAll('#procedure .skill')
+  skills && skills.forEach((skill) => {
+    skill.onclick = () => {
+      skill.classList.toggle('active')
+    }
+  })
+}
+page_doanh_nghiep_procedure()
+
