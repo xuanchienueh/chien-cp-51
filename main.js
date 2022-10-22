38067c6ecd73d1c711b4ad8c4e464ffb26ee8e3f
xuanchienueh <xuanchien.ueh@gmail.com>
2022-10-22 21:06:21 +0700

    save

--- main.js
+++ main.js
@@ -1,35 +1,49 @@
 const scrollContainer = document.querySelector("div.container");
-const elementScroll = document.querySelectorAll('.container section')
+const chieucao = scrollContainer.offsetHeight;
+console.log(scrollContainer.offsetTop);
+console.log("chieucao", chieucao);
+const elementScroll = document.querySelectorAll(".container section");
 var maxScrollLeft = scrollContainer.scrollWidth - scrollContainer.clientWidth;
-console.log('maxScrollLeft', maxScrollLeft)
+console.log("maxScrollLeft", maxScrollLeft);
+// let checkToView = scrollContainer.getBoundingClientRect().top;
+// console.log(checkToView);
 
-let x = 0
-let isWheel = true
+let x = 0;
+let isWheel = true;
 
 function onWheel(evt) {
   evt.preventDefault();
   scrollContainer.scrollLeft += evt.deltaY;
-  // console.log(evt)
-  x += evt.deltaY
+  scrollContainer.scrollIntoView();
 
-  // console.log(scrollContainer.scrollLeft);
-  console.log(x)
-  if (x > maxScrollLeft || x === 0) {
-    pauseWheel()
+  x += evt.deltaY;
+
+  console.log(scrollContainer.scrollLeft);
+  // console.log(x);
+  if (Math.floor(scrollContainer.scrollLeft) == maxScrollLeft || x <= 0) {
+    pauseWheel();
   }
 }
-scrollContainer.onmouseover = () => {
-  isWheel = true
-  console.log(243)
-}
-isWheel && scrollContainer.addEventListener("wheel", onWheel, { smooth: true });
-
-
-
-
-
+// isWheel && scrollContainer.addEventListener("wheel", onWheel, { smooth: true });
+window.onscroll = () => {
+  const html = document.documentElement;
+  // console.log(html.scrollTop);
+  // if (
+  //   html.scrollTop >= scrollContainer.offsetTop + chieucao ||
+  //   html.scrollTop < scrollContainer.offsetTop
+  // ) {
+  //   scrollContainer.addEventListener("wheel", onWheel, { smooth: true });
+  // }
+  if (
+    html.scrollTop > scrollContainer.offsetTop - 400 &&
+    html.scrollTop < scrollContainer.offsetTop + chieucao
+  ) {
+    scrollContainer.addEventListener("wheel", onWheel, { smooth: true });
+  }
+};
 
 function pauseWheel() {
-  scrollContainer.removeEventListener('wheel', onWheel)
-  isWheel = false
+  scrollContainer.removeEventListener("wheel", onWheel);
+  isWheel = false;
+  console.log("pauseWheel run");
 }
