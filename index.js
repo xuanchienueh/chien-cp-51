d7c43fdd4f767ab8a7e40b8dd1ffb64dc79b9b05
xuanchienueh <xuanchien.ueh@gmail.com>
2022-11-12 11:09:01 +0700

    save

--- index.js
+++ index.js
@@ -1,40 +1,22 @@
-let datHang = new Promise((resolve, reject) => {
-    // resolve('dat hang thanh cong')
-    reject(' nhan hang fail')
-
-})
-let nhanHang = new Promise((resolve, reject) => {
-    resolve('nhận hàng thanh cong')
-})
-let boomHang = new Promise((resolve, reject) => {
-    resolve('boom hang thanh cong')
-})
-// datHang.then((res) => {
-//     console.log(res)
-//     return nhanHang
-// }).then((res) => {
-//     console.log(res)
-//     return boomHang
-// }).then((res) => {
-//     console.log(res)
-// })
-
-// let ham = async () => {
-//     try {
-//         let dathang = await datHang
-//         let nhanhang = await nhanHang
-//         console.log(dathang)
-//         console.log(nhanhang)
-//     } catch (err) {
-//         console.log(err)
-//     }
-
-
-// }
-// ham()
-
-Promise.allSettled([datHang, nhanHang, boomHang]).then((resolve, reject) => {
-    console.log(resolve)
-}).catch((err) => {
-    console.log(err)
-})
+var testimonialThumbs = new Swiper(".testimonial-thumbs", {
+    spaceBetween: 10,
+    slidesPerView: 3,
+    centeredSlides: true,
+    freeMode: true,
+    autoplay: true,
+    loop: true,
+    speed: 3000
+});
+var testimonialContent = new Swiper(".testimonial-comment", {
+    spaceBetween: 10,
+    autoplay: true,
+    loop: true,
+    speed: 3000,
+    thumbs: {
+        swiper: testimonialThumbs
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+    }
+});
