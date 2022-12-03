b6ea4bf9ea1bd62ae0d6cff5bc6cc67edbdb9bc0
xuanchienueh <xuanchien.ueh@gmail.com>
2022-12-03 08:27:33 +0700

    save

--- limit_10_tag_blog.js
+++ limit_10_tag_blog.js
@@ -1,6 +1,9 @@
 const listButton = document.querySelectorAll("#myBar .number");
+const listContent = document.querySelectorAll(".content .item");
+const boxContent = document.querySelector(".main .content");
 const myBar = document.querySelector("#myBar");
 const widthItem = 100 / (listButton.length + 1);
+let indexActive = 0;
 myBar.style.width = `${widthItem}%`;
 listButton.forEach((ele, i) => {
   listButton[i].style.left = `calc(${widthItem * (i + 1)}% - 30px)`;
@@ -17,14 +20,18 @@ function move(e) {
   // lấy index của listButton
   const rows = Array.from(listButton);
   let index = rows.indexOf(e.currentTarget);
+  indexActive = index;
   // active xanh khi click
   listButton[index].classList.add("active");
+
   listButton.forEach((ele, i) => {
     ele.classList.remove("active_arrow");
+    listContent[i].classList.remove("active");
   });
   listButton[index].classList.add("active_arrow");
+  listContent[index].classList.add("active");
 
-  let id = setInterval(frame, 1);
+  let id = setInterval(frame, 5);
   function frame() {
     if (index * widthItem < percent_prev) {
       listButton.forEach((ele, i) => {
@@ -55,3 +62,26 @@ function move(e) {
     percent_prev = (index + 1) * widthItem;
   }, 300);
 }
+
+let timerInterval = setInterval(() => {
+  if (indexActive >= listButton.length - 1) {
+    indexActive = 0;
+  } else {
+    indexActive++;
+  }
+  listButton[indexActive].click();
+}, 1500);
+
+boxContent.onmouseover = () => {
+  clearInterval(timerInterval);
+};
+boxContent.onmouseout = () => {
+  timerInterval = setInterval(() => {
+    if (indexActive >= listButton.length - 1) {
+      indexActive = 0;
+    } else {
+      indexActive++;
+    }
+    listButton[indexActive].click();
+  }, 1500);
+};
